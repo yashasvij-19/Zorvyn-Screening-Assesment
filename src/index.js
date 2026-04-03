835dd516feec9e8fb6f690fd11839e66336e449e
yashasvij-19 <yashasvivij01@gmail.com>
2026-04-03 19:00:47 +0530

    add role based permissions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-require ('./db/database')
+require("./db/database");
 
-app.use(express.json())
+app.use(express.json());
 
-const authRoutes = require('./routes/auth')
-app.use('/api/auth',authRoutes)
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
 
-const {authenticate} = require('./middleware/auth')
-app.get('/api/protected', authenticate, (req,res) => {
-  res.json({message:'You are in',user:req.user})
+const { authenticate, authorize } = require('./middleware/auth')
+app.get('/api/protected', authenticate, authorize('admin'), (req, res) => {
+  res.json({ message: 'You are in', user: req.user })
 })
 
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' })
-})
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
